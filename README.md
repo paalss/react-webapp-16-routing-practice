633d12d4b669275d0e16671ff4409e328c754705
paalss <paalss94@gmail.com>
2021-06-11 18:01:19 +0200

    mulig å toggle sortering for quote-listen

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # react-webapp-routing-practice
 
 ![app](src/assets/app-2021-06-11-at-141044.png)
+![app](src/assets/app-2021-06-11-at-17.54.52.png)
 
 git add . && git commit -m "" && git push origin main
--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,11 +1,45 @@
+import { useState } from "react";
+import Card from "../components/UI/Card";
 import QuoteList from "./../components/quotes/QuoteList";
 import NoQuotesFound from "./../components/quotes/QuoteList";
 
 const AllQuotes = ({ quotes }) => {
-  // console.log("ALLQUOTES RUNNING");
+  const [isAscending, setIsAscending] = useState(true);
+
+  let buttonText = isAscending ? "Ascending" : "descending";
+
+  const clickHandler = () => {
+    setIsAscending((prevState) => {
+      return !prevState;
+    });
+  };
+
+  let sortedQuotes = "";
+  if (isAscending) {
+    sortedQuotes = quotes.sort((a, b) =>
+      a.text > b.text ? 1 : b.text > a.text ? -1 : 0
+    );
+  } else {
+    sortedQuotes = quotes.sort((b, a) =>
+      a.text > b.text ? 1 : b.text > a.text ? -1 : 0
+    );
+  }
+
   const content =
-    quotes.length !== 0 ? <QuoteList quotes={quotes} /> : <NoQuotesFound />;
-  return <div>{content}</div>;
+    quotes.length !== 0 ? (
+      <QuoteList quotes={sortedQuotes} />
+    ) : (
+      <NoQuotesFound />
+    );
+
+  return (
+    <Card>
+      <button className="btn" onClick={clickHandler}>
+        {buttonText}
+      </button>
+      {content}
+    </Card>
+  );
 };
 
 export default AllQuotes;
